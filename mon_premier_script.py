91f3527d0ff8361a6353b336c8ececa78ebb11d1
Timestorm2099 <157361450+Timestorm2099@users.noreply.github.com>
2024-02-11 10:39:51 +0000

    Clean Code de la fonction names"

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -1,23 +1,24 @@
 import unittest
+from typing import List
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print("Prenom supérieur à 7 : " + prenom)
-        else:
-            print("Prenom inférieur ou égal à 7 : " + prenom)
-    return more_than_seven
+#Count names with more than seven letters
 
-class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
+class NamesCounter:
+    def count_names(prenoms: List[str]) -> int:
+        more_than_seven_count: int = 0
+        for prenom in prenoms:
+            if len(prenom) > 7:
+                more_than_seven_count += 1
+                print("Prenom supérieur à 7 : " + prenom)
+            else:
+                print("Prenom inférieur ou égal à 7 : " + prenom)
+        return more_than_seven_count
+
+class TestNamesCounter(unittest.TestCase):
+    def test_names_count(self):
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+        more_than_seven_count = NamesCounter.count_names(prenoms=prenoms)
+        self.assertEqual(more_than_seven_count, 4)
 
 if __name__ == '__main__':
     unittest.main()
